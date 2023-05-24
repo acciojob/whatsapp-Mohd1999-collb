e5e628ec6b62bf401a98504a24ac11badc93ed6c
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-05-24 10:26:30 +0000

    Done

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -1,7 +1,65 @@
+// package com.driver;
+
+// public class Group {
+//     private String name;
+//     private int numberOfParticipants;
+
+// }
+
 package com.driver;
 
+// import lombok.Getter;
+// import lombok.Setter;
+
+import java.util.Objects;
+
+// @Getter
+// @Setter
 public class Group {
     private String name;
     private int numberOfParticipants;
 
+    public Group(String name, int numberOfParticipants) {
+        this.name = name;
+        this.numberOfParticipants = numberOfParticipants;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfParticipants() {
+        return numberOfParticipants;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Group))
+            return false;
+        Group group = (Group) o;
+        return this.numberOfParticipants == group.numberOfParticipants && Objects.equals(this.name, group.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, numberOfParticipants);
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "name='" + name + '\'' +
+                ", numberOfParticipants=" + numberOfParticipants +
+                '}';
+    }
 }
--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -1,6 +1,57 @@
+// package com.driver;
+
+// public class User {
+//     private String name;
+//     private String mobile;
+// }
+
 package com.driver;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+// import lombok.Getter;
+// import lombok.Setter;
+
+import java.util.Objects;
+
+// @Getter
+// @Setter
 public class User {
     private String name;
     private String mobile;
+
+    public User(String name, String mobile) {
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getName(), user.getName()) && Objects.equals(getMobile(), user.getMobile());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getMobile());
+    }
 }
+
+
